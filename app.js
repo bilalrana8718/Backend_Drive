791955610a34fa3d7e4405d792a929846873bdba
Rana Bilal Akbar <bilal.rana8718@gmail.com>
2024-12-25 00:39:44 +0500

    Route

--- app.js
+++ app.js
@@ -3,18 +3,23 @@
 const express = require("express");
 const morgan = require('morgan');
 const userRouter = require('./routes/user.routes');
-
-
+const { body, validationResult } = require('express-validator');
 const app = express();
+
+app.use(express.json()); // JSON request logger
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+
 app.use(morgan('combined'));
 
 app.set('view engine', 'ejs');
 
-app.use('/users', userRouter);
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+    res.render('index');
 })
 
+app.use('/users', userRouter);
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000/')
 });
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
-// router.get('/test', (req, res) => {
-//     res.send("Hello World !!");
-// })
+router.get('/register', (req, res) => {
+    res.render("register");
+});
 
+router.post('/register',
+    body('email').trim().isEmail().isLength({ min: 13 }),
+    body('password').trim().isLength({ min: 8 }),
+    body('username').trim().isLength({ min: 5 }),
+    (req, res) => {
+        // do add the name attribute in the input field HTML code
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: "Invalid Inputs"
+            });
+        }
 
+        console.log(req.body);
+        res.send("User Registered Successfully");
+    });
 
 module.exports = router
